d8de893b6f217098f2dfcf33c5c2575d5d9acfae
alxc89 <alexcaardoso@gmail.com>
2021-11-19 00:08:48 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+using CursoCsharp.Fundamentos;
+
+namespace CursoCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
