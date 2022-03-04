45ffe40e355b8d1826ffb7b1e9c3acde8c9e0dec
alxc89 <alexcaardoso@gmail.com>
2022-03-03 23:23:57 -0300

    OO: Herança

---
+++ OO/Heranca.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoCsharp.OO
+{
+    public class Carro
+    {
+        public readonly int VelocidadeMaxima;
+
+        int VelocidadeAtual;
+
+        public Carro(int velocidadeMaxima)
+        {
+            VelocidadeMaxima = velocidadeMaxima;
+        }
+
+        protected int AlterarVelocidade(int delta)
+        {
+            int novaVelocidade = VelocidadeAtual + delta;
+
+            if (novaVelocidade < 0)
+            {
+                VelocidadeAtual = 0;
+            }
+            else if (novaVelocidade > VelocidadeMaxima)
+            {
+                VelocidadeAtual = VelocidadeMaxima;
+            }
+            else
+            {
+                VelocidadeAtual = novaVelocidade;
+            }
+
+            return VelocidadeAtual;
+        }
+
+        public int Acelerar()
+        {
+            return AlterarVelocidade(5);
+        }
+
+        public int Frear()
+        {
+            return AlterarVelocidade(-5);
+        }
+    }
+
+    public class Uno : Carro
+    {
+        public Uno() : base(200) { }
+    }
+
+    public class Ferrari : Carro
+    {
+        public Ferrari() : base(300) { }
+    }
+
+    public class Heranca
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Uno...");
+
+            Uno carro1 = new Uno();
+
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Frear());
+            Console.WriteLine(carro1.Frear());
+            Console.WriteLine(carro1.Frear());
+
+            Ferrari carro2 = new Ferrari();
+
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Frear());
+            Console.WriteLine(carro1.Frear());
+            Console.WriteLine(carro1.Frear());
+        }
+    }
+}
